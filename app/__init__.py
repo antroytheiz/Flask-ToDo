a341a5ff3caa678b66073b342798784f9aff341c
Theiz <45439030+antroytheiz@users.noreply.github.com>
2020-09-12 11:20:18 +0900

    change db from mysql to sqlite

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = 'thisismysecret'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://theis:salupa@localhost/flasktodo'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo-mysql-db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
